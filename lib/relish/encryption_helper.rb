239e8a2c7d08d6aa0f4a2648cd6aff8bc1d158bf
Blake Gentry <blakesgentry@gmail.com>
2012-10-10 15:34:48 -0700

    refactor encryption helper. add upgrade_env_encryption

--- lib/relish/encryption_helper.rb
+++ lib/relish/encryption_helper.rb
@@ -1,3 +1,4 @@
+require "relish/release"
 require "fernet"
 require "openssl"
 
@@ -11,21 +12,35 @@ class Relish
       @secrets = secrets
     end
 
+    def encrypt_env(env)
+      encrypt_key_with_secret("env", env, hmac_secrets.first)
+    end
+
+    def decrypt_env(encrypted_token)
+      try_decrypt_with_index(encrypted_token)[0]
+    end
+
+    def upgrade_env_encryption(encrypted_token)
+      if encrypted_token.nil? || encrypted_token == ""
+        return false, encrypted_token
+      end
+      env, i = try_decrypt_with_index(encrypted_token)
+      i == 0 ? [false, encrypted_token] : [true, encrypt_env(env)]
+    end
+
+    protected
+
     def hmac_secrets
       @hmac_secrets ||= @secrets.map do |secret|
         OpenSSL::HMAC.hexdigest('sha256', @static_secret, secret)
       end
     end
 
-    def encrypt_env(env)
-      encrypt_key_with_secret("env", env, hmac_secrets.first)
-    end
-
-    def decrypt_env(encrypted_token)
+    def try_decrypt_with_index(encrypted_token)
       hmac_secrets.each_with_index do |secret, i|
         success, env = try_decrypt(secret, encrypted_token, "env")
         if success
-          return env
+          return env, i
         end
       end
       raise RelishDecryptionFailed
@@ -36,8 +51,6 @@ class Relish
     rescue OpenSSL::Cipher::CipherError => e
       return false, {}
     end
-
-    protected
 
     def decrypt_key(secret, encrypted_token, hash_key)
       verifier = Fernet.verifier(secret, encrypted_token)
