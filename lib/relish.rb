6975a282bcbec474caf3f9af0c6408239cb47f46
Joshua Tobin <joshuatobin@gmail.com>
2014-10-02 19:16:06 -0400

    Removing begin since we're not rescuing

--- lib/relish.rb
+++ lib/relish.rb
@@ -12,51 +12,44 @@ class Relish
 
   def copy(id, version, data)
     rescue_dynamodb_error do
-      begin
-        Release.new.tap do |release|
-          release.item = {}
-          release.id = id
-          release.version = version
-          data.each do |k, v|
-            release.send("#{k}=", v.to_s) unless v.nil?
-          end
-          @db.put(release.item)
+      Release.new.tap do |release|
+        release.item = {}
+        release.id = id
+        release.version = version
+        data.each do |k, v|
+          release.send("#{k}=", v.to_s) unless v.nil?
         end
+        @db.put(release.item)
       end
     end
   end
 
   def create(id, data)
     rescue_dynamodb_error do
-      begin
-        item = @db.query_current_version(id)
-        Release.new.tap do |release|
-          if item.nil?
-            release.item = {}
-            release.id = id
-            release.version = "1"
-          else
-            release.item = item
-            release.version = (release.version.to_i + 1).to_s
-          end
-          data.each do |k, v|
-            release.send("#{k}=", v.to_s) unless v.nil?
-          end
-          @db.put_current_version(release.item)
+      item = @db.query_current_version(id)
+      Release.new.tap do |release|
+        if item.nil?
+          release.item = {}
+          release.id = id
+          release.version = "1"
+        else
+          release.item = item
+          release.version = (release.version.to_i + 1).to_s
         end
+        data.each do |k, v|
+          release.send("#{k}=", v.to_s) unless v.nil?
+        end
+        @db.put_current_version(release.item)
       end
     end
   end
 
-
   def current(id, *attrs)
     rescue_dynamodb_error do
-      begin
-        item = @db.query_current_version(id, *attrs)
-        unless item.nil?
-          Release.new.tap do |release|
-            release.item = item
-          end
+      item = @db.query_current_version(id, *attrs)
+      unless item.nil?
+        Release.new.tap do |release|
+          release.item = item
         end
       end
     end
@@ -64,24 +57,21 @@ class Relish
 
   def read(id, version, *attrs)
     rescue_dynamodb_error do
-      begin
-        item = @db.get_version(id, version, *attrs)
-        unless item.nil?
-          Release.new.tap do |release|
-            release.item = item
-          end
+      item = @db.get_version(id, version, *attrs)
+      unless item.nil?
+        Release.new.tap do |release|
+          release.item = item
         end
       end
     end
+  end
 
   def dump(id, consistent=nil, limit=nil)
     rescue_dynamodb_error do
-      begin
-        items = @db.query(id, consistent, limit)
-        items.map do |item|
-          Release.new.tap do |release|
-            release.item = item
-          end
+      items = @db.query(id, consistent, limit)
+      items.map do |item|
+        Release.new.tap do |release|
+          release.item = item
         end
       end
     end
@@ -89,16 +79,14 @@ class Relish
     
   def update(id, version, data)
     rescue_dynamodb_error do
-      begin
-        item = @db.get_version(id, version)
-        unless item.nil?
-          Release.new.tap do |release|
-            release.item = item
-            data.each do |k, v|
-              release.send("#{k}=", v.to_s) unless v.nil?
-            end
-            @db.put_version(id, version, release.item)
+      item = @db.get_version(id, version)
+      unless item.nil?
+        Release.new.tap do |release|
+          release.item = item
+          data.each do |k, v|
+            release.send("#{k}=", v.to_s) unless v.nil?
           end
+          @db.put_version(id, version, release.item)
         end
       end
     end
@@ -106,11 +94,8 @@ class Relish
 
   def delete(id, version)
     rescue_dynamodb_error do
-      begin
         @db.delete_version(id, version)
-      end
     end
   end
-
 
 end
