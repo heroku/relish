aa47cde357760189355b30bae53076c84e5fcdae
Michael Hale <mhale@heroku.com>
2018-11-09 10:04:57 -0500

    extract common variables, formatting

--- spec/relish_encryption_helper_spec.rb
+++ spec/relish_encryption_helper_spec.rb
@@ -1,18 +1,17 @@
 require 'spec_helper'
-require 'fernet/legacy'
 
 describe Relish::EncryptionHelper do
-  before do
-  end
+  let(:good_secret) { 'a' * 32    }
+  let(:bad_secret)  { 'b' * 32    }
+  let(:key)         { 'my-key'    }
+  let(:data)        { 'test-data' }
 
   describe '#decrypt' do
     it 'raises RelishDecryptionFailed on incorrect decryption secret' do
-      secret = '89f057fc596f2419e83a1e15a42d7fde503edc23d3674655cabd5c5d6ad7ae61' 
-      data = 'test-data'
-      key = 'my-data' 
-      encrypt_helper = Relish::EncryptionHelper.new('static_secret', [secret])
-      decrypt_helper = Relish::EncryptionHelper.new('static_secret', ['a' * 32])
+      encrypt_helper = Relish::EncryptionHelper.new('static_secret', [good_secret])
+      decrypt_helper = Relish::EncryptionHelper.new('static_secret', [bad_secret])
       token = encrypt_helper.encrypt(key, data)
+
       assert_raises(RelishDecryptionFailed) do
         decrypt_helper.decrypt(key, token)
       end
@@ -20,17 +19,11 @@ describe Relish::EncryptionHelper do
 
     it 'ignores keys that raise JSON errors' do
       allow(Fernet::Legacy).to receive(:verifier).and_call_original
-
-      good = 'a'*32
-      bad = 'b'*32
-
-      encrypt_helper = Relish::EncryptionHelper.new('static_secret', [good])
-      data = 'test-data'
-      key = 'my-data' 
+      encrypt_helper = Relish::EncryptionHelper.new('static_secret', [good_secret])
       token = encrypt_helper.encrypt(key, data)
 
-      decrypt_helper = Relish::EncryptionHelper.new('static_secret', [bad, good])
-      bad_hmac = OpenSSL::HMAC.hexdigest('sha256', 'static_secret', bad)
+      decrypt_helper = Relish::EncryptionHelper.new('static_secret', [bad_secret, good_secret])
+      bad_hmac = OpenSSL::HMAC.hexdigest('sha256', 'static_secret', bad_secret)
       expect(Fernet::Legacy).to \
         receive(:verifier).with(bad_hmac, token).and_raise(MultiJson::ParseError)
 
