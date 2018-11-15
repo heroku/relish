4c2af8778f123862c8ffb1273152d49a2ba1cbfe
Michael Hale <mhale@heroku.com>
2018-11-15 11:07:30 -0500

    Encryption helper encrypts as current, decrypts legacy, or current

--- lib/relish/encryption_helper.rb
+++ lib/relish/encryption_helper.rb
@@ -1,5 +1,6 @@
 require "relish/release"
 require "fernet/legacy"
+require "fernet"
 require "openssl"
 
 class RelishDecryptionFailed < RuntimeError; end
@@ -13,18 +14,27 @@ class Relish
     end
 
     def encrypt(key, value)
+      current_encrypt(key, value)
+    end
+
+    def current_encrypt(_key, value)
+      Fernet.generate(hmac_secrets.first[0, 32], value)
+    end
+
+    def legacy_encrypt(key, value)
       Fernet::Legacy.generate(hmac_secrets.first) do |gen|
-        gen.data = {key => value}
+        gen.data = { key => value }
       end
     end
 
     def decrypt(key, token)
+      plain = nil
       hmac_secrets.each do |secret|
-        if verifier = verifier(secret, token)
-          return verifier.data[key] if verifier.valid?
-        end
+        plain = decrypt_with_secret(secret, token, key)
+        break if plain
       end
-      raise RelishDecryptionFailed
+      raise RelishDecryptionFailed unless plain
+      plain
     end
 
     def upgrade(key, token)
@@ -40,6 +50,10 @@ class Relish
 
     alias to_s inspect
 
+    def legacy?(token)
+      !!(token =~ /.+?\|.+?\|.+?/)
+    end
+
     protected
 
     def hmac_secrets
@@ -48,15 +62,23 @@ class Relish
       end
     end
 
-    def verifier(secret, token)
-      Fernet::Legacy.verifier(secret, token).tap do |verifier|
-        verifier.enforce_ttl = false
-        verifier.verify_token(token)
+    def decrypt_with_secret(secret, token, key)
+      legacy = legacy?(token)
+      verifier = if legacy
+        Fernet::Legacy.verifier(secret, token)
+      else
+        Fernet.verifier(secret[0, 32], token)
       end
+
+      verifier.enforce_ttl = false
+      verifier.verify_token(token) if legacy
+      return nil unless verifier.valid?
+
+      legacy ? verifier.data[key] : verifier.message
     rescue OpenSSL::Cipher::CipherError
-    # Certain combinations of keys and encrypted data cause decryption with an
-    # incorrect key to succeed (no CipherError) but produce garbage data which
-    # cannot be decoded into JSON, and thus fail with a ParseError instead.
+      # Certain combinations of keys and encrypted data cause decryption with an
+      # incorrect key to succeed (no CipherError) but produce garbage data which
+      # cannot be decoded into JSON, and thus fail with a ParseError instead.
     rescue MultiJson::ParseError
     end
   end
--- spec/relish_encryption_helper_spec.rb
+++ spec/relish_encryption_helper_spec.rb
@@ -5,6 +5,7 @@ describe Relish::EncryptionHelper do
   let(:bad_secret)  { 'b' * 32    }
   let(:key)         { 'my-key'    }
   let(:data)        { 'test-data' }
+  let(:encrypt_helper) { Relish::EncryptionHelper.new('static_secret', [good_secret]) }
 
   describe '#decrypt' do
     it 'raises RelishDecryptionFailed on incorrect decryption secret' do
@@ -18,16 +19,31 @@ describe Relish::EncryptionHelper do
     end
 
     it 'ignores keys that raise JSON errors' do
-      allow(Fernet::Legacy).to receive(:verifier).and_call_original
-      encrypt_helper = Relish::EncryptionHelper.new('static_secret', [good_secret])
+      allow(Fernet).to receive(:verifier).and_call_original
       token = encrypt_helper.encrypt(key, data)
-
       decrypt_helper = Relish::EncryptionHelper.new('static_secret', [bad_secret, good_secret])
-      bad_hmac = OpenSSL::HMAC.hexdigest('sha256', 'static_secret', bad_secret)
-      expect(Fernet::Legacy).to \
-        receive(:verifier).with(bad_hmac, token).and_raise(MultiJson::ParseError)
 
+      expect(Fernet).to receive(:verifier).and_raise(MultiJson::ParseError)
       assert_equal data, decrypt_helper.decrypt(key, token)
     end
+  end
+
+  context "upgrading" do
+    it "reads data encrypted with legacy fernet" do
+      legacy_token = encrypt_helper.legacy_encrypt('foo', 'bar')
+      assert_equal 'bar', encrypt_helper.decrypt('foo', legacy_token)
+    end
+
+    it "reads data encrypted with non-legacy fernet" do
+      token = encrypt_helper.current_encrypt('foo', 'bar')
+      assert_equal 'bar', encrypt_helper.decrypt('foo', token)
+    end
+
+    it "writes data encrypted with non-legacy fernet" do
+      token = encrypt_helper.encrypt('foo', 'bar')
+      assert_equal false, encrypt_helper.legacy?(token)
+    end
+
+    it "includes a key name as cipher meta data"
   end
 end
