768ccb1f33fb4a6fd7ef500339b836048ce90d30
Fun1hero <dima220v@gmail.com>
2022-06-08 13:40:53 -0700

    tests for array of static secrets

--- spec/relish/encryption_helper_spec.rb
+++ spec/relish/encryption_helper_spec.rb
@@ -4,7 +4,7 @@ describe Relish::EncryptionHelper do
   let(:secret_one)     { 'a' * 32    }
   let(:secret_two)     { 'b' * 32    }
   let(:data)           { 'test-data' }
-  let(:encrypt_helper) { Relish::EncryptionHelper.new('static_secret', [secret_one]) }
+  let(:encrypt_helper) { Relish::EncryptionHelper.new('static_secret1', [secret_one]) }
 
   let(:encrypt_helper_array) { Relish::EncryptionHelper.new(['static_secret1','static_secret2'], ['a' * 32, 'c' * 32])}
   let(:secret_encrypt) {['b' * 32, 'd' * 32]}
@@ -12,7 +12,7 @@ describe Relish::EncryptionHelper do
   describe '#decrypt' do
     it 'raises RelishDecryptionFailed on incorrect decryption secret' do
       token = encrypt_helper.encrypt(data)
-      decrypt_helper = Relish::EncryptionHelper.new('static_secret', [secret_two])
+      decrypt_helper = Relish::EncryptionHelper.new('static_secret1', [secret_two])
 
       assert_raises(RelishDecryptionFailed) do
         decrypt_helper.decrypt(token)
@@ -21,13 +21,12 @@ describe Relish::EncryptionHelper do
 
     it 'decrypts data encrypted with a previous secret' do
       token = encrypt_helper.encrypt(data)
-      decrypt_helper = Relish::EncryptionHelper.new('static_secret', [secret_two, secret_one])
+      decrypt_helper = Relish::EncryptionHelper.new('static_secret1', [secret_two, secret_one])
 
       assert_equal data, decrypt_helper.decrypt(token)
     end
 
     it 'raises RelishDecryptionFailed on incorrect decryption for array of secrets' do
-      # token = encrypt_helper.encrypt(data)
       token = encrypt_helper_array.encrypt(data)
       decrypt_helper_array = Relish::EncryptionHelper.new(['static_secret1','static_secret2'], secret_encrypt)
 
@@ -36,12 +35,31 @@ describe Relish::EncryptionHelper do
       end
     end
 
+    it 'decrypts data encrypted with an array as secret' do
+      token = encrypt_helper_array.encrypt(data)
+      assert_equal data, encrypt_helper.decrypt(token)
+    end
+
+    it 'decrypts data encrypted with a string as secret' do
+      token = encrypt_helper.encrypt(data)
+      assert_equal data, encrypt_helper_array.decrypt(token)
+    end
+
+    it 'decrypts data encrypted with a previous array of secrets' do
+      token = encrypt_helper_array.encrypt(data)
+      assert_equal data, encrypt_helper_array.decrypt(token)
+    end
   end
 
   describe '#encrypt' do
     it 'encrypts' do
       token = encrypt_helper.encrypt(data)
       assert_equal data, encrypt_helper.decrypt(token)
+    end
+
+    it 'encrypts with array of secrets' do
+      token = encrypt_helper_array.encrypt(data)
+      assert_equal data, encrypt_helper_array.decrypt(token)
     end
   end
 end
